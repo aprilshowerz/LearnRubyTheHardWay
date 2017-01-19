15cb59e7a64d68415b8988b9220cbdcf57cfa6cb
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-18 22:16:50 -0500

    added comments

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,46 @@
+# sets varible types_of_people equal to 10
 types_of_people = 10
+
+# defines variable x.
 x = "There are #{types_of_people} types of people."
+
+# defines variable binary.
 binary = "binary"
+
+# defines variable do_not.
 do_not = "don't"
+
+# defines variable y.
 y = "Those who know #{binary} and those who #{do_not}."
 
+
+# writes the value of variable x to screen.
 puts x
+
+# writes the value of variable y to screen.
 puts y
 
+# writes to screen "I said: There are #{types_of_people} types of people.."
 puts "I said: #{x}."
+
+# writes to screen "I also said: Those who know #{binary} and those who #{do_not}.
 puts "I also said: '#{y}'."
 
+# sets variable hilarious equal to false.
 hilarious = false
+
+# sets variable joke_evaluation equal to "Isn't that joke so funny?! #{hilarious}"
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# writes value of joke_evaluation to screen. (false)
 puts joke_evaluation
 
+#sets variable w equal to "This is the left side of..."
 w = "This is the left side of..."
+
+# sets variable e equal to "a string with a right side."
 e = "a string with a right side."
 
+# concatenates values of variables w and e and writes to screen.
+# "This is the left side of...a string with a right side.
 puts w + e
