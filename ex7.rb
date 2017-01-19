8798a72b30cada3dc564e0f197d516155feea44d
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-18 22:44:05 -0500

    added comments

--- ex7.rb
+++ ex7.rb
@@ -1,21 +1,54 @@
+# writes to screen "Mary had a little lamb."
 puts "Mary had a little lamb."
+
+# writes to screen "Its fleece was white as snow."
 puts "Its fleece was white as #{'snow'}."
+
+# writes to screen "And everywhere that Mary went."
 puts "And everywhere that Mary went."
+
+# writes 10 periods to screen.
 puts "." * 10  # what'd that do?
 
+# sets end1 equal to C
 end1 = "C"
+
+# sets end2 equal to h
 end2 = "h"
+
+# sets end3 equal to e
 end3 = "e"
+
+# sets end4 equal to e
 end4 = "e"
+
+# sets end5 equal to s
 end5 = "s"
+
+# sets end6 equal to e
 end6 = "e"
+
+# sets end7 equal to B
 end7 = "B"
+
+# sets end8 equal to u
 end8 = "u"
+
+# sets end9 equal to r
 end9 = "r"
+
+# sets end10 equal to g
 end10 = "g"
+
+# sets end11 equal to e
 end11 = "e"
+
+# sets end12 equal to r
 end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
+
+# It concatenates the string values of the variables and prints them to screen.
+# print does not end the line. Puts ends the line.
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
