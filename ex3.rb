5ec5cfdcdab32405b69ebb09738129b516918afa
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-18 21:18:42 -0500

    changed integers to float

--- ex3.rb
+++ ex3.rb
@@ -2,28 +2,28 @@
 puts "I will now count my chickens:"
 
 #writes "Hens" and also the answer of a calculation. (30)
-puts "Hens #{25 + 30 / 6}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
 
 #writes "Roosters" and also the answer of a calculation. (97)
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 #writes "Now I will count the eggs:"
 puts "Now I will count the eggs:"
 
 #writes the answer to the calculation (which is 7)
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 #writes "Is it true that 3+2<5-7?"
 puts "Is it true that 3 + 2 < 5 - 7?"
 
 #determines if calculation is true or false and displays choice to screen.
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 #writes "What is 3 + 2?", calculates answer and displays it.
-puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{3.0 + 2.0}"
 
 #writes "What is 5 - 7?", calculates answer and displays it.
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
 
 #writes "Oh, that's why it's false."
 puts "Oh, that's why it's false."
@@ -32,10 +32,10 @@ puts "Oh, that's why it's false."
 puts "How about some more."
 
 # writes "Is it greater?", determines if calculation is true or false and displays choice to screen.
-puts "Is it greater? #{5 > -2}"
+puts "Is it greater? #{5.0 > -2.0}"
 
 # writes "Is it greater or equal?", determines if calculation is true or false and displays choice to screen.
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
 
 # writes "Is it less or equal?", determines if calculation is true or false and displays choice to screen.
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
