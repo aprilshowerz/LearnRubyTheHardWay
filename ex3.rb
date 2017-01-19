ea7f18810ddfa0b577507767eae9826bf5298eac
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-18 21:11:06 -0500

    initial commit, added comments

---
+++ ex3.rb
@@ -0,0 +1,41 @@
+#writes the text "I will now count my chickens:"
+puts "I will now count my chickens:"
+
+#writes "Hens" and also the answer of a calculation. (30)
+puts "Hens #{25 + 30 / 6}"
+
+#writes "Roosters" and also the answer of a calculation. (97)
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#writes "Now I will count the eggs:"
+puts "Now I will count the eggs:"
+
+#writes the answer to the calculation (which is 7)
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#writes "Is it true that 3+2<5-7?"
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#determines if calculation is true or false and displays choice to screen.
+puts 3 + 2 < 5 - 7
+
+#writes "What is 3 + 2?", calculates answer and displays it.
+puts "What is 3 + 2? #{3 + 2}"
+
+#writes "What is 5 - 7?", calculates answer and displays it.
+puts "What is 5 - 7? #{5 - 7}"
+
+#writes "Oh, that's why it's false."
+puts "Oh, that's why it's false."
+
+#writes "How about some more."
+puts "How about some more."
+
+# writes "Is it greater?", determines if calculation is true or false and displays choice to screen.
+puts "Is it greater? #{5 > -2}"
+
+# writes "Is it greater or equal?", determines if calculation is true or false and displays choice to screen.
+puts "Is it greater or equal? #{5 >= -2}"
+
+# writes "Is it less or equal?", determines if calculation is true or false and displays choice to screen.
+puts "Is it less or equal? #{5 <= -2}"
